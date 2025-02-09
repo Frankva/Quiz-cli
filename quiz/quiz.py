0709367070acdd8bf88fc5b361c498441779bd6b
frankva <24254885+Frankva@users.noreply.github.com>
2025-02-09 19:07:25 +0100

    TDD iteration 3: G ajoute une vérification du nombre de flashcard

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -11,6 +11,7 @@ class Quiz:
             self.__quiz_data = json.load(jsonfile)
         self.__check_format()
         self.__invoke_convert_quiz_data()
+        self.__check_is_valid()
 
     def __invoke_convert_quiz_data(self) -> None:
         self.__flashcards = tuple(map(
@@ -28,9 +29,16 @@ class Quiz:
             if 'verso' not in question:
                 self.__raise_json_format_error()
 
+    def __check_is_valid(self):
+        if not self.is_valid():
+            raise Exception('Erreur il y a trop de flashcard.')
+
     def get_count_flashcards(self) -> int:
         return len(self.__flashcards)
 
     @staticmethod
     def __raise_json_format_error():
         raise Exception('Erreur le json n’est pas formaté correctement.')
+
+    def is_valid(self):
+       return 1 <= self.get_count_flashcards() <= 30
