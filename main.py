e0488d562db3c8803eb67cdd6938252855a9f9b9
frankva <24254885+Frankva@users.noreply.github.com>
2025-02-09 15:33:26 +0100

    TDD iteration 1: G charge le json

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from quiz.quiz import quiz
 
+if __name__ == '__main__':
+    DEFAULT_JSON_FILE_NAME = './quiz.json'
+    quiz = quiz()
+    quiz.run(DEFAULT_JSON_FILE_NAME)
--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,3 +1,20 @@
+import json
+
 class quiz:
-    def run(filename):
-        pass
+    def __init__(self):
+        self.quiz_data: list = list()
+
+    def run(self, filename):
+        with open(filename) as jsonfile:
+            self.quiz_data = json.load(jsonfile)
+        self.__check_format()
+
+    def __check_format(self):
+        if not isinstance(self.quiz_data, list):
+            raise Exception('Erreur le json n’est pas formaté correctement.')
+        for question in self.quiz_data:
+            if 'recto' not in question:
+                raise Exception('Erreur le json n’est pas formaté correctement.')
+            if 'verso' not in question:
+                raise Exception('Erreur le json n’est pas formaté correctement.')
+
